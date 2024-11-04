2d4e15a84df9609c445d8c101d7eb1fd18617c7d
hbashar434 <h.bashar434@gmail.com>
2024-11-04 14:32:30 +0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT: number = parseInt(process.env.PORT as string, 10) || 8000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World! Server is running...');
+});
+
+// Start the server
+app.listen(PORT, (): void => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
