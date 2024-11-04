b81de8c8346302f94461ba4546839f996473a699
hbashar434 <h.bashar434@gmail.com>
2024-11-04 18:15:06 +0600

    add error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,35 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import userRoutes from './features/user/userRoutes';
 import dotenv from 'dotenv';
+import { ApiError } from './utils/ApiError';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT as string, 10) || 8000;
 
-// Middleware
+// middleware
 app.use(express.json());
 
-// Routes
+// user routes
+app.use('/api/users', userRoutes);
+
+// root route
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World! Server is running...');
 });
 
+// error handling middleware
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({ success: false, message: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
 
-
-// Start the server
+// start the server
 app.listen(PORT, (): void => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
